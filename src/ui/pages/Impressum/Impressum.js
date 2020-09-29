5d3000f206df786ca1e7af8ff1d037a8951b4eda
Edes Sulce <guerrilsi46@gmail.com>
2020-09-29 21:47:18 +0200

    Cleanup

--- src/ui/pages/Impressum/Impressum.js
+++ src/ui/pages/Impressum/Impressum.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Box } from '../../components/Base/Base';
-import Layout from '../../templates/Layout';
 import TextAdress from './sections/TextAdress';
 import TextPrivacyPolicy from './sections/TextPrivacyPolicy';
 import TextTermsOfService from './sections/TextTermsOfService';
@@ -8,13 +7,11 @@ import TextSecurityOverview from './sections/TextSecurityOverview';
 
 export default function Impressum() {
     return (
-        <Layout>
-            <Box lineHeight="1.3rem" mt="2rem" p="0 15px">
-                <TextAdress />
-                <TextTermsOfService />
-                <TextPrivacyPolicy />
-                <TextSecurityOverview />
-            </Box>
-        </Layout>
+        <Box lineHeight="1.3rem" mt="2rem" p="0 15px">
+            <TextAdress />
+            <TextTermsOfService />
+            <TextPrivacyPolicy />
+            <TextSecurityOverview />
+        </Box>
     );
 }
