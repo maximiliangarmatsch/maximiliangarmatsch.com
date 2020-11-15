9b0806e51ae3678eaaa0aaa0d1f49b5615978ba4
Edes Sulce <edessulce@outlook.com>
2020-11-15 18:18:35 +0100

    Bugs

--- src/ui/pages/ForYouIWill/ForYouIWillBody.js
+++ src/ui/pages/ForYouIWill/ForYouIWillBody.js
@@ -3,20 +3,20 @@ import { useTranslation } from 'react-i18next';
 import { Box, Text } from '../../components/Base/Base';
 import SectionCheckpoint from './components/SectionCheckpoint';
 
-const messageValues = {
-    First: chunks => (
-        <Text color="green" fontSize="23px" lineHeight="18px" fontWeight="400">
-            {chunks}
-        </Text>
-    ),
-    Green: chunks => <Text color="green">{chunks}</Text>,
-    linebreak: <br />,
-    a: chunks => (
-        <Text as="a" textDecoration="underline" href="#" color="green">
-            {chunks}
-        </Text>
-    ),
-};
+// const messageValues = {
+//     First: chunks => (
+//         <Text color="green" fontSize="23px" lineHeight="18px" fontWeight="400">
+//             {chunks}
+//         </Text>
+//     ),
+//     Green: chunks => <Text color="green">{chunks}</Text>,
+//     linebreak: <br />,
+//     a: chunks => (
+//         <Text as="a" textDecoration="underline" href="#" color="green">
+//             {chunks}
+//         </Text>
+//     ),
+// };
 
 export default function ForYouIWillBody() {
     const { t } = useTranslation();
@@ -37,30 +37,30 @@ export default function ForYouIWillBody() {
         >
             <SectionCheckpoint dataTestId="section-checkpoint">
                 {t(
-                    `${(
-                        <messageValues.First>Understand</messageValues.First>
-                    )} your needs ${(
-                        <messageValues.Green>in days.</messageValues.Green>
-                    )} My budget- and time-estimations will allow smart planning and decision making!`
+                    `
+                        Understand
+                    your needs 
+                        in days.
+                 My budget- and time-estimations will allow smart planning and decision making!`
                 )}
             </SectionCheckpoint>
             <SectionCheckpoint dataTestId="section-checkpoint">
                 {t(`
-                        ${(
-                            <messageValues.First>Create</messageValues.First>
-                        )} a prototype
-                        ${(
-                            <messageValues.Green>in weeks.</messageValues.Green>
-                        )}
+                        
+                            Create
+                         a prototype
+                        
+                            in weeks.
+                    
                         I write all initial code myself and guarantee a clean software architecture that we can build on!`)}
             </SectionCheckpoint>
             <SectionCheckpoint dataTestId="section-checkpoint">
-                {t(`${(
-                    <messageValues.First>Manage</messageValues.First>
-                )} your project
-                        ${(
-                            <messageValues.Green>long-term</messageValues.Green>
-                        )}.
+                {t(`
+                    Manage
+            your project
+                        
+                            long-term
+                        .
                         You want to continuously add components and scale your product?
                         I will hire, train and lead the digital team that will do it!`)}
             </SectionCheckpoint>
